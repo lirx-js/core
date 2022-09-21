9a71c6850a447899bff20931c1754e475ba1c8d8
valentin <valentin.richard@informaniak.com>
2022-09-21 02:51:18 +0200

    1.1.0-beta.1:update let$$ and add experimental map-observable-to-observer

--- src/observer-observable-pair/build-in/source/built-in/replay-last-source/derived/let.shortcut.ts
+++ src/observer-observable-pair/build-in/source/built-in/replay-last-source/derived/let.shortcut.ts
@@ -1,3 +1,21 @@
-export {
-  createMulticastReplayLastSource as let$$,
-} from './create-multicast-replay-last-source';
+import { IObservable } from '../../../../../../observable/type/observable.type';
+import { IObserver } from '../../../../../../observer/type/observer.type';
+import { ICreateReplayLastSourceInitialValue } from '../create-replay-last-source';
+import { createMulticastReplayLastSource } from './create-multicast-replay-last-source';
+
+export type ILetTuple<GValue> = [
+  emit: IObserver<GValue>,
+  subscribe: IObservable<GValue>,
+  getValue: () => GValue,
+];
+
+export function let$$<GValue>(
+  ...initialValue: ICreateReplayLastSourceInitialValue<GValue>
+): ILetTuple<GValue> {
+  const { emit, subscribe, getValue } = createMulticastReplayLastSource<GValue>(...initialValue);
+  return [
+    emit,
+    subscribe,
+    getValue,
+  ];
+}
