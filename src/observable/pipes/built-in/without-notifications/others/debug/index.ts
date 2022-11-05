d935e142e59f04c4fa3f36559370da3da31ec4f5
valentin <valentin.richard@informaniak.com>
2022-11-05 19:04:35 +0100

    - various fixes in the documentation - add some new experimental observables

--- src/observable/pipes/built-in/without-notifications/others/debug/index.ts
+++ src/observable/pipes/built-in/without-notifications/others/debug/index.ts
@@ -1,6 +1,6 @@
-export * from './log-state-observable.shortcut';
-export * from './log-state-observable';
-export * from './log-state-observable-pipe.shortcut';
-export * from './log-state-observable-pipe';
+export * from './debug-observable.shortcut';
+export * from './debug-observable';
+export * from './debug-observable-pipe.shortcut';
+export * from './debug-observable-pipe';
 
 
