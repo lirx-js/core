f5c1f99d493a2b39c8801e6a388400b7a461ef66
Valentin Richard <valentin.richard@infomaniak.com>
2024-03-03 11:23:47 +0100

    docs(website): improve home page

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import clsx from 'clsx';
+import React from 'react';
 import styles from './styles.module.css';
 
 type FeatureItem = {
@@ -11,11 +11,13 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Fast and Small',
-    Svg: require('@site/static/img/illustrations/undraw/red/undraw_outer_space_re_u9vd.svg').default,
+    Svg: require('@site/static/img/illustrations/undraw/red/undraw_outer_space_re_u9vd.svg')
+      .default,
     description: (
       <>
-        LiRX/core was designed from the ground up to be fast, small, and performant.
-        It <a href={'/docs/documentation/performances/'}>outperforms RxJS</a> in every aspects.
+        LiRX/core was designed from the ground up to be fast, small, and performant. It{' '}
+        <a href={'/docs/documentation/performances/introduction/'}>outperforms</a> concurrent
+        frameworks in many aspects.
       </>
     ),
   },
@@ -24,8 +26,8 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/illustrations/undraw/red/undraw_mind_map_re_nlb6.svg').default,
     description: (
       <>
-        Assemble many Observables to create custom data streams,
-        and adopt <i>Reactive Programming</i> for each of your async data sources.
+        Assemble many Observables to create custom data streams, and adopt{' '}
+        <i>Reactive Programming</i> for each of your async data sources.
       </>
     ),
   },
@@ -34,18 +36,21 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/illustrations/undraw/red/undraw_programmer_re_owql.svg').default,
     description: (
       <>
-        LiRX/core can create understandable and complex data pipelines with just a few lines of code.
-        Master data streams like a boss.
+        LiRX/core can create understandable and complex data pipelines with just a few lines of
+        code. Master data streams like a boss.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <Svg
+          className={styles.featureSvg}
+          role="img"
+        />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
@@ -61,7 +66,10 @@ export default function HomepageFeatures(): JSX.Element {
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+            <Feature
+              key={idx}
+              {...props}
+            />
           ))}
         </div>
       </div>
